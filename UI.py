0215c983221a72aa54953a07019664e34679ab2e
Nandan N <114895869+Nandan-N@users.noreply.github.com>
2024-04-23 19:27:35 +0530

    final frrrr

--- UI.py
+++ UI.py
@@ -8,43 +8,15 @@ from PIL import Image
 from pymongo import MongoClient
 from streamlit_feedback import streamlit_feedback
 
-st.set_page_config(page_title="Justice", page_icon="📒")
-st.title("Justice Justified ⚖️")
-st.header("Built by - Gautham K, Gautham P A, Nandan N, Shreya C.")
+st.set_page_config(page_title="Law", page_icon="📒")
+st.title("NLP RAG project ⚖️")
+st.header("Built by - Nandan N, Vibha M, Utkarsh S.")
 
 client = MongoClient("mongodb://localhost:27017/")
 mydatabase = client['Stats'] 
 mycollection = mydatabase['likes_and_dislikes'] 
 mycollection3=mydatabase['Failures']
-mycollection2=mydatabase['questions']
-# def fimage(file,pg):
-#   size=[]
-#   pdf_file = fitz.open(file)
-#   list=[]
-#   try:
-#     for page_index in range(pg,pg+1):
-#       page = pdf_file[page_index]
-#       image_list = page.get_images()
-#       if image_list:
-#         print(
-#           f"[+] Found a total of {len(image_list)} images in page {page_index}")
-#       else:
-#         print("[!] No images were found on page", page_index)
-#       for image_index, img in enumerate(page.get_images(), start=1):
-#         xref = img[0]
-#         base_image = pdf_file.extract_image(xref)
-#         image_bytes = base_image["image"]
-#         image_ext = base_image["ext"]
-#         image = Image.open(io.BytesIO(image_bytes))
-#         height,width=image.size
-#         size.append(height+width)
-        
-#         list.append(f"image{page_index+1}_{image_index}.{image_ext}")
-#         image.save(open(f"./image{page_index+1}_{image_index}.{image_ext}", "wb"))
-#   except:
-#     pass
-      
-#   return list,size    
+mycollection2=mydatabase['questions'] 
 
 
 if 'files' not in st.session_state:    
